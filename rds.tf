87819618ea9e7c60d14bd0b1f38d07c4d62aa779
brasil.arelsa <brasil.arelsa@gmail.com>
2026-07-20 12:00:47 -0300

    fix: RDS em subnets publicas para acesso externo

--- rds.tf
+++ rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_subnet_group" "oficina" {
   name       = "oficina-db-subnet"
-  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+  subnet_ids = [aws_subnet.public_a.id, aws_subnet.public_b.id]
 
   tags = { Name = "oficina-db-subnet-group" }
 }
